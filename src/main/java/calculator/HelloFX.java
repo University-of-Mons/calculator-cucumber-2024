865469f9d7aec6024ef024e6bc4d0fdadbf0aebc
Faltmatix <adrien.vandekerckhove@outlook.com>
2024-03-05 16:00:16 +0100

    Added Javafx

---
+++ src/main/java/calculator/HelloFX.java
@@ -0,0 +1,49 @@
+package calculator;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
+public class HelloFX extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start2(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/calculator-design.fxml"));
+
+        Scene scene = new Scene(root, 400, 800);
+        stage.setTitle("Calculator App");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
