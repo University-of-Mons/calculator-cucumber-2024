9f088e3043a169d1c04dda13c284baaca947db44
jika <vandechat@tutanota.com>
2024-03-16 20:46:01 +0100

    Made gui runnable through maven and jar working with gui

---
+++ src/main/java/gui/GuiApplication.java
@@ -0,0 +1,27 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class GuiApplication extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/calculator-design.fxml")));
+
+        Scene scene = new Scene(root, 400, 800);
+        stage.setTitle("Calculator App");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
