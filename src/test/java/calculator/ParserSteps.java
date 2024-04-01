17730ac540b8c86ff94b3bdb20451f47e81e52ca
JordanDemaret <210805@umons.ac.be>
2024-04-01 13:56:38 +0200

    test Cucumber for parser

---
+++ src/test/java/calculator/ParserSteps.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class ParserSteps {
+
+	private Calculator c;
+	private Parser inputParser;
+	@Given("I have a number {string}")
+	public void givenIHaveANumber(String s) {
+		c = new Calculator();
+		inputParser = new Parser(s, c);
+	}
+	@Given("I have an expression {string}")
+	public void givenIHaveAnExpression(String s) {
+		c = new Calculator();
+		inputParser = new Parser(s, c);
+	}
+
+	@Then("I should get a number {int}")
+	public void thenIShouldGetANumber(int number) {
+		assertEquals(number, inputParser.evaluate().getValue());
+	}
+	@Then("the result is  {int}")
+	public void thenTheResultIs(int result) {
+		assertEquals(result, inputParser.evaluate().getValue());
+	}
+
+}
