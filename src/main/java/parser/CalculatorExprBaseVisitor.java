2488d6a7a2c0c148d04dbb9d5d344dbedb61638f
MarineB210 <79014465+MarineB210@users.noreply.github.com>
2024-04-05 13:41:31 +0200

    Fix errors

--- src/main/java/parser/CalculatorExprBaseVisitor.java
+++ src/main/java/parser/CalculatorExprBaseVisitor.java
@@ -15,28 +15,24 @@ public class CalculatorExprBaseVisitor<T> extends AbstractParseTreeVisitor<T> im
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitProg(CalculatorExprParser.ProgContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitExp(CalculatorExprParser.ExpContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitIntInfix(CalculatorExprParser.IntInfixContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitParensInfix(CalculatorExprParser.ParensInfixContext ctx) { return visitChildren(ctx); }
 	/**
@@ -50,70 +46,60 @@ public class CalculatorExprBaseVisitor<T> extends AbstractParseTreeVisitor<T> im
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitAddSubInfix(CalculatorExprParser.AddSubInfixContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitDivInfix(CalculatorExprParser.DivInfixContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitMulDivPrefix(CalculatorExprParser.MulDivPrefixContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitAddSubPrefix(CalculatorExprParser.AddSubPrefixContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitIntPrefix(CalculatorExprParser.IntPrefixContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitParensPrefix(CalculatorExprParser.ParensPrefixContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitMulDivPostfix(CalculatorExprParser.MulDivPostfixContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitAddSubPostfix(CalculatorExprParser.AddSubPostfixContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitIntPostfix(CalculatorExprParser.IntPostfixContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitParensPostfix(CalculatorExprParser.ParensPostfixContext ctx) { return visitChildren(ctx); }
 }
--- src/main/java/parser/CalculatorExprVisitor.java
+++ src/main/java/parser/CalculatorExprVisitor.java
@@ -5,109 +5,93 @@ import org.antlr.v4.runtime.tree.ParseTreeVisitor;
  * This interface defines a complete generic visitor for a parse tree produced
  * by {@link CalculatorExprParser}.
  *
- * @param <T> The return type of the visit operation. Use {@link Void} for
- * operations with no return type.
+ * @param <T> The return type of the visit operation.
  */
 public interface CalculatorExprVisitor<T> extends ParseTreeVisitor<T> {
 	/**
-	 * Visit a parse tree produced by {@link CalculatorExprParser#prog}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitProg(CalculatorExprParser.ProgContext ctx);
 	/**
-	 * Visit a parse tree produced by {@link CalculatorExprParser#exp}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitExp(CalculatorExprParser.ExpContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code intInfix}
-	 * labeled alternative in {@link CalculatorExprParser#}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitIntInfix(CalculatorExprParser.IntInfixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code parensInfix}
-	 * labeled alternative in {@link CalculatorExprParser#}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitParensInfix(CalculatorExprParser.ParensInfixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code MulInfix}
-	 * labeled alternative in {@link CalculatorExprParser#}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitMulInfix(CalculatorExprParser.MulInfixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code AddSubInfix}
-	 * labeled alternative in {@link CalculatorExprParser#}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitAddSubInfix(CalculatorExprParser.AddSubInfixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code DivInfix}
-	 * labeled alternative in {@link CalculatorExprParser#}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitDivInfix(CalculatorExprParser.DivInfixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code MulDivPrefix}
-	 * labeled alternative in {@link CalculatorExprParser#prefix}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitMulDivPrefix(CalculatorExprParser.MulDivPrefixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code AddSubPrefix}
-	 * labeled alternative in {@link CalculatorExprParser#prefix}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitAddSubPrefix(CalculatorExprParser.AddSubPrefixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code intPrefix}
-	 * labeled alternative in {@link CalculatorExprParser#prefix}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitIntPrefix(CalculatorExprParser.IntPrefixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code parensPrefix}
-	 * labeled alternative in {@link CalculatorExprParser#prefix}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitParensPrefix(CalculatorExprParser.ParensPrefixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code MulDivPostfix}
-	 * labeled alternative in {@link CalculatorExprParser#postfix}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitMulDivPostfix(CalculatorExprParser.MulDivPostfixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code AddSubPostfix}
-	 * labeled alternative in {@link CalculatorExprParser#postfix}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitAddSubPostfix(CalculatorExprParser.AddSubPostfixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code intPostfix}
-	 * labeled alternative in {@link CalculatorExprParser#postfix}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
 	T visitIntPostfix(CalculatorExprParser.IntPostfixContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code parensPostfix}
-	 * labeled alternative in {@link CalculatorExprParser#postfix}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
