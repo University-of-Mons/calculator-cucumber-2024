f97724ac32eadf485d703a9eb279af863c69efc6
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2024-04-17 15:53:02 +0200

    Added operations in AbstractValue. Yet to be implemented in the other classes

--- src/main/java/back/calculator/types/RealValue.java
+++ src/main/java/back/calculator/types/RealValue.java
@@ -84,6 +84,31 @@ public class RealValue extends AbstractValue{
         return new RealValue(this.value.sqrt(this.precision));
     }
 
+    @Override
+    public AbstractValue cos() {
+        return null;
+    }
+
+    @Override
+    public AbstractValue sin() {
+        return null;
+    }
+
+    @Override
+    public AbstractValue ln() {
+        return null;
+    }
+
+    @Override
+    public AbstractValue exp() {
+        return null;
+    }
+
+    @Override
+    public AbstractValue atan() {
+        return null;
+    }
+
 
     @Override
     public boolean isEqualsZero() {
