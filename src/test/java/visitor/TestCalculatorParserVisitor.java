9092d6702a6cba67bd8b931d241cf0c65b8d816a
LicorneRose765 <210376@umons.ac.be>
2024-04-21 12:45:57 +0200

    Add mock :sunglasses: for parserVisitor (I was not able to test everything :pensive:)

--- src/test/java/visitor/TestCalculatorParserVisitor.java
+++ src/test/java/visitor/TestCalculatorParserVisitor.java
@@ -4,12 +4,21 @@ package visitor;
 import back.calculator.Calculator;
 import back.calculator.Expression;
 import back.calculator.Notation;
+import back.calculator.types.NotANumber;
+import back.parser.calculatorParser;
+import back.visitor.CalculatorParserVisitor;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestCalculatorParserVisitor {
+
+    private CalculatorParserVisitor visitor;
+    private calculatorParser.IntAtomContext atom;
 
     private Calculator calc;
 
@@ -21,10 +30,34 @@ class TestCalculatorParserVisitor {
 
     @BeforeEach
     void setUp() {
+        visitor = new CalculatorParserVisitor();
+
+        atom = Mockito.mock(calculatorParser.IntAtomContext.class);
+        TerminalNode node = Mockito.mock(TerminalNode.class);
+        Mockito.when(node.getText()).thenReturn("5");
+        Mockito.when(atom.NUMBER()).thenReturn(node);
+
         calc = new Calculator();
         value1 = 8;
         value2 = 6;
         value3 = 5;
+    }
+
+
+    @Test
+    void testGetExpressionReturnsNotANumberForInvalidToken() {
+        calculatorParser.MulDivInfixContext ctx = Mockito.mock(calculatorParser.MulDivInfixContext.class);
+        Token token = Mockito.mock(Token.class);
+        Mockito.when(token.getType()).thenReturn(-1);
+        ctx.op = token;
+        // Add params to ctx
+        Mockito.when(ctx.getChild(0)).thenReturn(atom);
+        Mockito.when(ctx.getChild(2)).thenReturn(atom);
+        Token goodToken = Mockito.mock(Token.class);
+        Mockito.when(goodToken.getType()).thenReturn(1);
+
+
+        assertEquals(new NotANumber(), visitor.visitMulDivInfix(ctx));
     }
 
     @Test
@@ -66,7 +99,7 @@ class TestCalculatorParserVisitor {
     @Test
     void testParseParensPrefix() {
         String expression = "/ (2, 5)";
-        Expression e = calc.read("("+expression+")");
+        Expression e = calc.read("(" + expression + ")");
         assertEquals(expression, calc.format(e, Notation.PREFIX));
     }
 
@@ -91,7 +124,7 @@ class TestCalculatorParserVisitor {
     @Test
     void testParseParensPostfix() {
         String expression = "(2, 5) /";
-        Expression e = calc.read("("+expression+")");
+        Expression e = calc.read("(" + expression + ")");
         assertEquals(expression, calc.format(e, Notation.POSTFIX));
     }
 
